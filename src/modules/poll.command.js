d4476a105d78d966ba2251d1a63cb31f38c018c8
Timofey Nazarov <timothy96@rambler.ru>
2018-12-17 18:19:10 +0300

    styles fix
    minor style fixes, indentation, comments etc.

--- src/modules/poll.command.js
+++ src/modules/poll.command.js
@@ -7,25 +7,24 @@ const command = require('./command');
  * quotes are used to ignore spaces inside the answers
  */
 const poll = async function(response, { input, i18n }) {
-	poll_obj = {};
+    poll_obj = {};
 
-	//separating the question by question mark
-	poll_obj.question = input.substring(input.indexOf(' ') + 1 , input.indexOf('?'));
-	// poll_obj.question = cutBetween(input, ' ', '?').cutString;
+    // separating the question by question mark
+    poll_obj.question = input.substring(input.indexOf(' ') + 1 , input.indexOf('?'));
 
-	//the rest of string in split into answers, spaces inside quotes are ignored
+    // the rest of string in split into answers, spaces inside quotes are ignored
+    // TODO strip the quotes ('/") from the answers
     poll_obj.answers = input.substring(input.indexOf('?') + 1).replace(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g, '|').split('|')
    
-    //getting rid ot the empty string elements, if there are any
+    // getting rid ot the empty string elements, if there are any
     poll_obj.answers = poll_obj.answers.filter(function (el){
         return el != '';
     });;
     
-    //TODO connect to the database and get real generated ID
+    // TODO connect to the database and get real generated ID
     poll_obj.pollId = Math.floor(Math.random() * 100);
 
     response.output = JSON.stringify(poll_obj, null, 2);
-
     return response;
 };
 
