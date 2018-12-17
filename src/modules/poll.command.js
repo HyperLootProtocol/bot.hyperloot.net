afabf18ac8e6552e5aa5da752d3758ae7351882a
Timofey Nazarov <timothy96@rambler.ru>
2018-12-17 18:32:56 +0300

    quotes are stripped from the answers added

--- src/modules/poll.command.js
+++ src/modules/poll.command.js
@@ -12,9 +12,8 @@ const poll = async function(response, { input, i18n }) {
     // separating the question by question mark
     poll_obj.question = input.substring(input.indexOf(' ') + 1 , input.indexOf('?'));
 
-    // the rest of string in split into answers, spaces inside quotes are ignored
-    // TODO strip the quotes ('/") from the answers
-    poll_obj.answers = input.substring(input.indexOf('?') + 1).replace(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g, '|').split('|');
+    // the rest of string in split into answers, spaces inside quotes are ignored, quotes are then stripped
+    poll_obj.answers = input.substring(input.indexOf('?') + 1).replace(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g, '|').replace(/['"]/g, "").split('|');
    
     // getting rid ot the empty string elements, if there are any
     poll_obj.answers = poll_obj.answers.filter(function (el){
