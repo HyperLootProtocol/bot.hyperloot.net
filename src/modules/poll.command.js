b46580d18a4bfc1ece647096190a49dc5a9f41d5
Timofey Nazarov <timothy96@rambler.ru>
2018-12-19 15:02:02 +0300

    styles fix (with help of lint)

--- src/modules/poll.command.js
+++ src/modules/poll.command.js
@@ -6,24 +6,25 @@ const command = require('./command');
  * input line for creating a poll should look like /poll <question sentence>? <answer1> <"answer 2"> ...
  * quotes are used to ignore spaces inside the answers
  */
-const poll = async function(response, { input, i18n }) {
-    poll_obj = {};
+const poll = async function (response, { input }) {
+    const pollObj = {};
 
     // separating the question by question mark
-    poll_obj.question = input.substring(input.indexOf(' ') + 1 , input.indexOf('?'));
+    pollObj.question = input.substring(input.indexOf(' ') + 1, input.indexOf('?'));
 
     // the rest of string in split into answers, spaces inside quotes are ignored, quotes are then stripped
-    poll_obj.answers = input.substring(input.indexOf('?') + 1).replace(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g, '|').replace(/['"]/g, "").split('|');
-   
-    // getting rid ot the empty string elements, if there are any
-    poll_obj.answers = poll_obj.answers.filter(function (el){
-        return el != '';
-    });
-    
-    // TODO connect to the database and get real generated ID
-    poll_obj.pollId = Math.floor(Math.random() * 100);
+    pollObj.answers = input.substring(input.indexOf('?') + 1).replace(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g, '|').replace(/['"]/g, '').split('|');
 
-    response.output = JSON.stringify(poll_obj, null, 2);
+    // getting rid ot the empty string elements, if there are any
+    pollObj.answers = pollObj.answers.filter(el => el !== '');
+
+    // TODO connect to the database and get real generated ID
+    pollObj.pollId = Math.floor(Math.random() * 100);
+
+    // TODO add status open/closed (accepting votes), maybe need to store creator id to only let him/her close poll?
+    // create in memory objects to test votes and other operations with polls
+
+    response.output = JSON.stringify(pollObj, null, 2);
     return response;
 };
 
