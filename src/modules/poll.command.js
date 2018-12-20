e387f72b08ef0986b40dd457991f6639610ca7a3
Timofey Nazarov <timothy96@rambler.ru>
2018-12-20 19:31:47 +0300

    polls response output implemented (placeholders for votes for now)

--- src/modules/poll.command.js
+++ src/modules/poll.command.js
@@ -32,6 +32,17 @@ const getPolls = function (id) {
     return polls;
 };
 
+const getPollStringInfo = function (_poll) {
+    let _output = `${_poll.question} | ${'?'} votes | ID${_poll.pollId}\n`;
+    for (let i = 0; i<_poll.answers.length; i++) {
+        _output += `${_poll.answers[i]} (??%)`;
+        if (i< _poll.answers.length - 1){
+            _output += ' | '
+        };
+    };
+    return _output;
+};
+
 /**
  * poll() implements the polls logic
  * input line for creating a poll should look like /poll 'question sentence' answer1 'answer 2' ...
@@ -51,16 +62,25 @@ const poll = async function (response, { id }) {
         // newly created poll is active (not closed)
         pollObj.active = 1;
 
+        // store the date the poll has been created on
+        pollObj.dateCreated = new Date();
+
         // TODO connect to the database and get real generated ID
         pollObj.pollId = Math.floor(Math.random() * 100);
-        response.output = JSON.stringify(pollObj, null, 2);
+
+        response.output = `Poll created successfully, vote by typing: /vote ${pollObj.pollId} your_answer`;
     } else if (response.rawArgs.length === 1) {
+        // the first and only arg is the polls id
         const pollId = response.rawArgs[0];
-        response.output = JSON.stringify(getPolls(pollId), null, 2);
+        const _poll = getPolls(pollId);
+
+        // output poll info
+        response.output = getPollStringInfo(_poll);
     } else {
-        console.log(getPolls());
-        response.output = JSON.stringify(getPolls(), null, 2);
-    }
+        for (let value of Object.keys(getPolls())) {
+            response.output += getPollStringInfo(getPolls(value)) + '\n\n';
+        };
+    };
 
     return response;
 };
