1fe13500379b2bf181ebcd7e97e61b6dbd2194ff
dcversus <dcversus@gmail.com>
2018-12-21 01:49:46 +0300

    db rework

--- src/modules/logText.js
+++ src/modules/logText.js
@@ -7,32 +7,28 @@ const conditions = [
     msg => msg.includes(' '),
 ];
 
-module.exports = async function (response, { input, db, id }) {
+module.exports = async function logText(response, context) {
+    const { input, setModuleData, getModuleData } = context;
     const msgFit = reduce(
         conditions,
         (acc, condition) => condition(input),
         true,
     );
 
-    // TODO one request/query
-    const updateQuery = {
-        $inc: {
-            'data.log.allCounter': 1,
-        },
-    };
+    const { allCounter, fitCounter } = await getModuleData('log', context);
 
-    const setQuery = {
-        $set: {
-            'data.log.lastMsgData': new Date(),
-        },
+    const query = {
+        allCounter: allCounter + 1,
+        lastMsgData: new Date(),
     };
 
     if (msgFit) {
-        extend(updateQuery.$inc, { 'data.log.fitCounter': 1 });
+        extend(query, {
+            fitCounter: fitCounter + 1,
+        });
     }
 
-    db.users.update({ discordId: id }, updateQuery);
-    db.users.update({ discordId: id }, setQuery);
+    await setModuleData('log', context, query);
 
     return response;
 };
