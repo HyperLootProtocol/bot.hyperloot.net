d73d81d7809ac91d40316ee373c0b07f586dc15c
Timofey Nazarov <timothy96@rambler.ru>
2018-12-28 13:43:00 +0300

    minor improvements

--- src/modules/poll/index.js
+++ src/modules/poll/index.js
@@ -1,17 +1,19 @@
 
 const command = require('../command');
-// const { discord: { broadcastChannelName } } = require('../../config');
 
 /**
  * reusable function to form an output string, containing the info about a single poll
  */
 const getPollStringInfo = function (i18n, {
+    getModuleData,
     dateCreated,
     question,
     options,
     pollId,
     isOpen,
 }) {
+    const { list = [] } = await getModuleData('poll');
+    
     const day = dateCreated.getDate();
     const month = dateCreated.getMonth();
 
