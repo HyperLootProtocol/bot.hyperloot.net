c8992986c2377c7fec5ba3baf1d9c14f9198e78c
Dmitriy <dim0k92sokol@gmail.com>
2019-01-10 18:43:49 +0200

    Now work with url on chat

--- src/modules/reactions-gif.js
+++ src/modules/reactions-gif.js
@@ -1,5 +1,5 @@
 const sample = require('lodash/sample');
-const { gifs } = require('./reaction-list.json');
+const gifs = require('./reaction-list.json');
 const { SOCIAL_PREFIX } = require('../config');
 
 const reactionSocial = async function (response, { id, input, i18n }) {
@@ -7,9 +7,21 @@ const reactionSocial = async function (response, { id, input, i18n }) {
         return response;
     }
 
-    const idWho = input.split(' ')[1].substr(2, input.length - 9);
+    if (input.indexOf(' ') > 0) {
+        const idWho = input.split(' ')[1].slice(2, -1);
+        const reactionName = input.split(' ')[0].substr(1);
+        const message = i18n(`reaction_${reactionName}`, { id, idWho });
+        const gif = sample(gifs[reactionName] || []);
+
+        if (message && gif) {
+            response.output = message + [gif];
+            console.log(message);
+        }
+        return response;
+    }
+
     const reactionName = input.split(' ')[0].substr(1);
-    const message = i18n(`reaction_${reactionName}`, { id, idWho });
+    const message = i18n(`reactionOne_${reactionName}`, { id });
     const gif = sample(gifs[reactionName] || []);
 
     if (message && gif) {
@@ -17,6 +29,7 @@ const reactionSocial = async function (response, { id, input, i18n }) {
         console.log(message);
     }
     return response;
+
 };
 
 module.exports = [reactionSocial];
