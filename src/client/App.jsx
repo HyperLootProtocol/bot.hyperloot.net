e80f9c1a7bbd62f58987cb75ba915a82c9ca1130
Ivan <kot91ivan@gmail.com>
2019-01-11 15:55:01 +0300

    add "Number of messages per day"

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Bar} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
 
+const _ = require('lodash');
 const API = '/api?query=getLog';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-
 
         this.state = {
             logs: [],
@@ -20,50 +21,67 @@ export default class App extends React.Component {
     }
 
     renderData(logs) {
+        console.log('logs', logs);
+        let dates = logs.reduce((prev, curr) => [...prev, curr.date.split('T')[0]], []); // Метод который обрабатывает элементы и создает массив из date; split('T')[0] - обрезает дату начиная с буквы Т;
+        // console.log(_.groupBy(dates, item => moment(item.date).format('YYYY-MM-DD')));
 
-        console.log('logs', logs)
+        // Считает количество дат в массиве
+        let countDates = dates.reduce((acc, el) => {
+            acc[el] = (acc[el] || 0) + 1;
+            return acc;
+        }, {});
+
+        //Функция для сортировки
+        function sortObject(item) {
+            return Object.keys(item)
+                .sort()
+                .reduce((a, v) => {
+                    a[v] = item[v];
+                    return a;
+                }, {});
+        }
+        //Сортируем дату
+        let sortDates = sortObject(countDates);
+
+        //Собирает даты с объекта countDates для оси Х
+        let xDate = Object.keys(sortDates).map(function(key) {
+            return String(key);
+        });
+
+        //Собирает количество дат с объекта countDates для оси У
+        let yDate = Object.keys(sortDates).map(function(key) {
+            return sortDates[key];
+        });
 
         return {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }
+            labels: xDate,
+            datasets: [
+                {
+                    label: 'Number of messages per day',
+                    data: yDate,
+                    backgroundColor: ['rgba(54, 162, 235, 0.4)'],
+                    borderColor: ['rgba(54, 162, 235, 1)'],
+                    borderWidth: 1,
+                    lineTension: 0.1,
+                },
+            ],
+        };
     }
 
     render() {
-
         return (
             <>
-                <p>React here! { this.state.logs.length ? 'loaded': 'loading' }</p>
+                <p>Here! {this.state.logs.length ? 'loaded' : 'loading'}</p>
 
-                <Bar
+                <Line
                     data={this.renderData(this.state.logs)}
                     width={100}
-                    height={50}
+                    height={100}
                     options={{
-                        maintainAspectRatio: false
+                        maintainAspectRatio: false,
                     }}
                 />
             </>
-        )
+        );
     }
 }
