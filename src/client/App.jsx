f5ec3e25fd47452eee3f0fdb2b94dd71455487df
Ivan <kot91ivan@gmail.com>
2019-01-14 19:01:52 +0300

    Add filter User chart

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import _ from 'lodash';
 import moment from 'moment';
 
-const _ = require('lodash');
 const API = '/api?query=getLog';
 
 export default class App extends React.Component {
@@ -21,46 +21,87 @@ export default class App extends React.Component {
     }
 
     renderData(logs) {
-        console.log('logs', logs);
-        let dates = logs.reduce((prev, curr) => [...prev, curr.date.split('T')[0]], []); // Метод который обрабатывает элементы и создает массив из date; split('T')[0] - обрезает дату начиная с буквы Т;
-        // console.log(_.groupBy(dates, item => moment(item.date).format('YYYY-MM-DD')));
-
-        // Считает количество дат в массиве
-        let countDates = dates.reduce((acc, el) => {
-            acc[el] = (acc[el] || 0) + 1;
-            return acc;
-        }, {});
-
-        //Функция для сортировки
-        function sortObject(item) {
-            return Object.keys(item)
-                .sort()
-                .reduce((a, v) => {
-                    a[v] = item[v];
-                    return a;
-                }, {});
-        }
-        //Сортируем дату
-        let sortDates = sortObject(countDates);
-
-        //Собирает даты с объекта countDates для оси Х
-        let xDate = Object.keys(sortDates).map(function(key) {
-            return String(key);
-        });
-
-        //Собирает количество дат с объекта countDates для оси У
-        let yDate = Object.keys(sortDates).map(function(key) {
-            return sortDates[key];
-        });
+        const groupedData = _.groupBy(logs, item => moment(item.date).format('DD.MM.YY'));
+        const labels = _.keys(groupedData).sort();
+        let data = [];
+        labels.forEach(key => data.push(groupedData[key].length));
 
         return {
-            labels: xDate,
+            labels,
             datasets: [
                 {
                     label: 'Number of messages per day',
-                    data: yDate,
+                    data,
                     backgroundColor: ['rgba(54, 162, 235, 0.4)'],
                     borderColor: ['rgba(54, 162, 235, 1)'],
+                    borderWidth: 1,
+                    lineTension: 0.1,
+                },
+            ],
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: true,
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+    }
+    renderDataHour(logs) {
+        const groupedData = _.groupBy(logs, item => moment(item.date).format('HH:00'));
+        const labels = _.keys(groupedData).sort();
+        let data = [];
+        labels.forEach(key => data.push(groupedData[key].length));
+
+        return {
+            labels,
+            datasets: [
+                {
+                    label: 'Number of messages per hour',
+                    data,
+                    backgroundColor: ['rgba(233, 235, 0, 0.4)'],
+                    borderColor: ['rgba(233, 235, 0, 1)'],
+                    borderWidth: 1,
+                    lineTension: 0.1,
+                },
+            ],
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: true,
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+    }
+    renderDataUser(logs) {
+        console.log('logs', logs);
+
+        const groupedUser = _.filter(logs, ['userId', '509115709704372243']);
+        const groupedData = _.groupBy(groupedUser, item => moment(item.date).format('DD.MM.YY'));
+        const labels = _.keys(groupedData).sort();
+        let data = [];
+        labels.forEach(key => data.push(groupedData[key].length));
+        console.log('Grouped Data:' + groupedData);
+        console.log('Data: ' + data);
+        console.log('Labels: ' + labels);
+
+        return {
+            labels,
+            datasets: [
+                {
+                    label: 'Number of messages User per day',
+                    data,
+                    backgroundColor: ['rgba(233, 235, 0, 0.4)'],
+                    borderColor: ['rgba(233, 235, 0, 1)'],
                     borderWidth: 1,
                     lineTension: 0.1,
                 },
@@ -75,10 +116,26 @@ export default class App extends React.Component {
 
                 <Line
                     data={this.renderData(this.state.logs)}
-                    width={100}
-                    height={100}
+                    width={80}
+                    height={20}
                     options={{
-                        maintainAspectRatio: false,
+                        maintainAspectRatio: true,
+                    }}
+                />
+                <Line
+                    data={this.renderDataHour(this.state.logs)}
+                    width={80}
+                    height={20}
+                    options={{
+                        maintainAspectRatio: true,
+                    }}
+                />
+                <Line
+                    data={this.renderDataUser(this.state.logs)}
+                    width={80}
+                    height={20}
+                    options={{
+                        maintainAspectRatio: true,
                     }}
                 />
             </>
