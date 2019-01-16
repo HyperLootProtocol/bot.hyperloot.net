db95cd79e217716165468cc6fd04f16c224620c3
Ivan <kot91ivan@gmail.com>
2019-01-16 18:05:51 +0300

    Add filter on ID and date range

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -12,17 +12,21 @@ export default class App extends React.Component {
 
         this.state = {
             logs: [],
-            axisX: [],
-            axisY: [],
-            selectedUsername: '',
-            selectedUserId: '',
             value: '',
+            selectedDate: '',
         };
         this.handleChange = this.handleChange.bind(this);
+        this.calendarChange = this.calendarChange.bind(this);
     }
+
     handleChange(event) {
         this.setState({ value: event.target.value });
     }
+
+    calendarChange(date) {
+        this.setState({ selectedDate: date });
+    }
+
     componentDidMount() {
         fetch(API)
             .then(response => response.json())
@@ -31,7 +35,18 @@ export default class App extends React.Component {
 
     renderData(logs) {
         // const groupedUser = _.filter(logs, [ userId: userIdVariable]); Для фильтра по userId
-        const groupedData = _.groupBy(logs, item => moment(item.date).format('DD.MM.YY'));
+        let Date = logs;
+        if (this.state.value) {
+            Date = _.filter(logs, { _id: this.state.value });
+        }
+
+        if (this.state.selectedDate) {
+            Date = _.filter(logs, item =>
+                moment(item.date).isBetween(this.state.selectedDate[0], this.state.selectedDate[1]),
+            );
+        }
+
+        const groupedData = _.groupBy(Date, item => moment(item.date).format('DD.MM.YY'));
         const labels = _.keys(groupedData).sort();
         let data = [];
         labels.forEach(key => data.push(groupedData[key].length));
@@ -97,9 +112,11 @@ export default class App extends React.Component {
         return (
             <>
                 <p>Here! {this.state.logs.length ? 'loaded' : 'loading'}</p>
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                <p onChange={this.handleChange}>{this.state.value}</p>
-                <Calendar />
+                <>
+                    <p>Поиск по ID:</p>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </>
+                <Calendar onChange={this.calendarChange} value={this.state.selectedDate} selectRange />
                 <Line
                     data={this.renderData(this.state.logs)}
                     width={80}
